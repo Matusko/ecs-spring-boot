9f83cfda4dace51acd21aff9cd7fc6f5f1ff7a77
matus <bartko.mat@gmail.com>
2018-04-28 23:02:20 +0200

    webhooks test

--- src/main/java/sk/matusko/ecs/MainController.java
+++ src/main/java/sk/matusko/ecs/MainController.java
@@ -8,6 +8,6 @@ public class MainController {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World changed again";
+        return "Hello Docker World, i added webhooks";
     }
 }
